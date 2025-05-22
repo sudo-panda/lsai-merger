1bc9f9cd64dbff91cd6e2599b6b30f42350c137a
Baidyanath Kundu <kundubaidya99@gmail.com>
2025-05-22 06:50:36 +0200

    Add sbatch file to benchmark the project

---
+++ bench-merged.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+#### Runs: train-bench.py
+#### Benchmarks: Forward, Backward passes every 5 steps
+####
+#### Notes:  1. If you want to bechmark all the steps find and replace
+####            `run_training ""` with `run_training "--logging-frequency 1"`
+
+#SBATCH --account=a-large-sc
+#SBATCH --time=00:45:00
+#SBATCH --job-name=lsai-proj
+#SBATCH --output=/iopsstor/scratch/cscs/bkundu/lsai-merger/logs/info/%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=72
+#SBATCH --mem=256G
+#SBATCH --partition=debug
+#SBATCH --environment=/iopsstor/scratch/cscs/bkundu/lsai-merger/ngc_pt_jan.toml     # Vanilla 25.01 PyTorch NGC Image 
+#SBATCH --no-requeue	# Prevent Slurm to requeue the job if the execution crashes (e.g. node failure) so we don't loose the logs
+
+# Exit immediately if a command exits with a non-zero status (good practice)
+set -eo pipefail
+
+echo "START TIME: $(date)"
+
+ASSIGNMENT_DIR="/iopsstor/scratch/cscs/$USER/lsai-proj"
+MERGER_DIR="/iopsstor/scratch/cscs/$USER/lsai-merger"
+
+cd $ASSIGNMENT_DIR
+
+if [ ! -d ".venv" ]; then
+    python -m venv --system-site-packages .venv
+fi
+
+source .venv/bin/activate
+
+export CUDA_HOME=${CUDA_HOME:-/usr/local/cuda}    # or whatever module set
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
+if pip show flash_attn_3 &> /dev/null; then
+    echo "flash_attn_3 is already installed"
+else
+    echo "Installing flash_attn_3..."
+    cd flash-attention/hopper
+    MAX_JOBS=8 pip install -e .
+    pip install torch-utils
+
+    # Becnhmark
+    python benchmark_mla_decode.py
+    cd -
+fi
+
+export PYTHONPATH=$PWD/flash-attention/hopper
+
+# nvidia-smi -i 0 --lock-gpu-clocks 1830,1830
+
+# # Set up ENV
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+FILE_SUFFIX="$SLURM_JOB_NAME-$SLURM_JOB_ID"
+
+cd $MERGER_DIR/LSAI-Checkpointing  # TODO: do everything in MERGER_DIR
+
+if [ ! -f "checkpointing/pccheck/libtest_ssd.so" ]; then
+    ./install.sh
+fi
+
+
+get_srun_outfile() {
+    local out_file_dir="$1"
+    local out_file_prefix="$2"
+    local out_file_suffix="$3"
+
+
+    if [ -z "$out_file_suffix" ]; then
+        TEMP_BENCH_NUMBER=1
+        srun_outfile="$out_file_dir/$out_file_prefix-$TEMP_BENCH_NUMBER.out"
+        while [ -f "$srun_outfile" ]; do
+            TEMP_BENCH_NUMBER=$((TEMP_BENCH_NUMBER + 1))
+            srun_outfile="$out_file_dir/$out_file_prefix-$TEMP_BENCH_NUMBER.out"
+        done
+    else
+        srun_outfile="$out_file_dir/$out_file_prefix-$out_file_suffix.out"
+    fi
+
+    echo "$srun_outfile"
+}
+
+run_merger() {
+    MODE="$1"
+    BENCH_NUMBER="$2"
+    TRAINING_ARGS="$3"
+
+    [ -d "$MERGER_DIR/chkpts" ] || mkdir $MERGER_DIR/chkpts
+    [ -d "$MERGER_DIR/results" ] || mkdir $MERGER_DIR/results
+
+    SRUN_OUTFILE_SUFFIX="merger"
+    CMD_PREFIX="numactl --membind=0-3"
+
+    if [ "$MODE" == "base" ]; then
+        TRAIN_FILE_NAME="training.train_checkp"
+    elif [ "$MODE" == "pccheck" ]; then
+        TRAIN_FILE_NAME="training.train_pccheck"
+    else
+        echo "Unknown mode: $MODE"
+        exit 1
+    fi
+
+    bench_flash_attn() {
+        local flash_attn=$1
+        local out_file_prefix=""
+        local srun_outfile=""
+
+        if [ "$flash_attn" == "True" ]; then
+            export USE_FLASH_ATTENTION="1"
+            out_file_prefix="$MODE-flash"
+            if [ "$MODE" == "base" ]; then
+                time_out=105
+            elif [ "$MODE" == "pccheck" ]; then
+                time_out=80
+            fi
+        else
+            export USE_FLASH_ATTENTION="0"
+            out_file_prefix="$MODE-torch"
+            
+            if [ "$MODE" == "base" ]; then
+                time_out=90
+            elif [ "$MODE" == "pccheck" ]; then
+                time_out=85
+            fi
+        fi
+
+        srun_outfile=$(get_srun_outfile "$MERGER_DIR/logs" "$SRUN_OUTFILE_SUFFIX-$out_file_prefix" "$BENCH_NUMBER-part1")
+
+        echo "Writing to: $srun_outfile"
+
+        DTYPE="bf16"
+        [ -d "$MERGER_DIR/results/$DTYPE" ] || mkdir $MERGER_DIR/results/$DTYPE
+
+        TRAINING_BASE_CMD="python3 -m $TRAIN_FILE_NAME \
+                              --seed 4 \
+                              --model-dtype $DTYPE \
+                              --checkpoint-dir $MERGER_DIR/chkpts \
+                              --checkpoint-freq 100"
+
+        TRAINING_CMD="$TRAINING_BASE_CMD \
+                              --loss-file $MERGER_DIR/results/$DTYPE/loss-$out_file_prefix-$BENCH_NUMBER-part1.csv \
+                              $TRAINING_ARGS"
+        srun --output $srun_outfile \
+             --cpus-per-task $SLURM_CPUS_PER_TASK \
+             bash -c "timeout -s SIGINT $time_out $CMD_PREFIX $TRAINING_CMD; ec=\$?; [ \$ec -eq 0 ] || [ \$ec -eq 124 ]"
+
+
+        
+        srun_outfile=$(get_srun_outfile "$MERGER_DIR/logs" "$SRUN_OUTFILE_SUFFIX-$out_file_prefix" "$BENCH_NUMBER-part2")
+
+        echo "Writing to: $srun_outfile"
+
+        TRAINING_CMD="$TRAINING_BASE_CMD \
+                              --loss-file $MERGER_DIR/results/$DTYPE/loss-$out_file_prefix-$BENCH_NUMBER-part2.csv \
+                              --load-checkpoint \
+                              $TRAINING_ARGS"
+        srun --output $srun_outfile \
+             --cpus-per-task $SLURM_CPUS_PER_TASK \
+             bash -c "$CMD_PREFIX $TRAINING_CMD"
+    }
+
+    bench_flash_attn "True"
+    bench_flash_attn "False"
+}
+
+
+SEQ_LEN=2048
+TRAIN_ARGS="--sequence-length $SEQ_LEN"
+
+run_merger "base" "$SEQ_LEN" "$TRAIN_ARGS"
+run_merger "pccheck" "$SEQ_LEN" "$TRAIN_ARGS"
+
+echo "END TIME: $(date)"
