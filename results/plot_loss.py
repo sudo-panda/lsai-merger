d13679a6f7e48e1df144a2904a4f2bc0a0972627
Baidyanath Kundu <kundubaidya99@gmail.com>
2025-05-23 11:55:43 +0200

    Add logs after FAv3 fix

--- results/plot_loss.py
+++ results/plot_loss.py
@@ -6,42 +6,46 @@ import matplotlib.pyplot as plt
 
 # pattern = 
 def plt_loss_curves(seq_len):
-    csv_files = [f"bf16/loss-base-torch-{seq_len}-part1.csv", f"bf16/loss-base-torch-{seq_len}-part2.csv", f"bf16/loss-pccheck-flash-{seq_len}-part1.csv", f"bf16/loss-pccheck-flash-{seq_len}-part2.csv"]
+    csv_files = glob.glob(f"bf16/loss-*-{seq_len}-part*.csv")
 
     # Plot the loss curves
     plt.figure(figsize=(8, 5))
+    restart_steps = {}
     for fname in sorted(csv_files):
         df = pd.read_csv(fname)
         if {"step", "loss"} <= set(df.columns):
             label = os.path.splitext(os.path.basename(fname))[0]
+
+            if "torch" in label:
+                color = 'gold'
+            else:
+                color = 'lime'
+            
             if "pccheck" in label:
                 lw = 1.5
                 dashes = (5, 5)
                 linestyle = 'dashed'
-                color = 'darkorange'
-                if "part1" in label:
-                    label = "PCcheck + FlashAttentionV3"
-                    pccheck_restart = df["step"].max()
-                else:
-                    label = None
+                color = 'darkorange' if color == 'gold' else 'darkgreen'
             else:
                 lw = 1.5
                 dashes = (5, 0)
                 linestyle = 'solid'
-                color = 'darkgreen'
-                if "part1" in label:
-                    label = "torch.save + SDPA"
-                    base_restart = df["step"].max()
-                else:
-                    label = None
+                    
+            
+            if "part1" in label:
+                label = label.replace(f"-{seq_len}-part1", "").replace("loss-", "")
+                restart_steps[label] = df["step"].max()
+            else:
+                label = None
 
             plt.plot(df["step"][::4], df["loss"][::4], label=label, linestyle=linestyle, dashes=dashes, lw=lw, color=color)
 
-    if base_restart == pccheck_restart:
-        plt.axvline(x = base_restart, color = 'red', label = 'Restart', linestyle='dashed')
+    # Check if all elements in restart_steps are the same
+    if all(x == list(restart_steps.values())[0] for x in restart_steps.values()):
+        plt.axvline(x=list(restart_steps.values())[0], color='red', label='Restart', linestyle='dashed')
     else:
-        plt.axvline(x = base_restart, color = 'orange', label = 'Restart (Base)', linestyle='dashed')
-        plt.axvline(x = pccheck_restart, color = 'green', label = 'Restart (PCcheck + FAv3)', linestyle='dashed')
+        for key, step in restart_steps.items():
+            plt.axvline(x=step, label=f'Restart {key}', linestyle='dashed')
     
     plt.xlabel("Step")
     plt.ylabel("Loss")
